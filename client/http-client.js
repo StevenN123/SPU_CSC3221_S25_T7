5c05a47434b4c5c9070124fa3a98e227e2938b2f
Sabina-Salimova <salimovas@spu.edu>
2025-06-04 20:18:25 -0700

    Update http-client.js

--- client/http-client.js
+++ client/http-client.js
@@ -1,58 +1,27 @@
 // http-client.js
+
 export default class HttpClient {
-  constructor(baseUrl = '') {
-    this.baseUrl = baseUrl.replace(/\/$/, ''); // Remove trailing slash
+  constructor(baseURL) {
+    this.baseURL = baseURL;
   }
 
-  async request(method, endpoint, { params = {}, query = {}, body = null } = {}) {
-    try {
-      // Replace route parameters
-      let url = endpoint.replace(/\/:(\w+)/g, (_, key) => `/${params[key]}`);
-      url = `${this.baseUrl}${url}`;
-
-      // Add query parameters
-      const queryStr = new URLSearchParams(query).toString();
-      if (queryStr) url += `?${queryStr}`;
-
-      const options = {
-        method,
-        headers: { 'Content-Type': 'application/json' }
-      };
-
-      if (body && ['POST', 'PUT', 'PATCH'].includes(method)) {
-        options.body = JSON.stringify(body);
-      }
-
-      const response = await fetch(url, options);
-      
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-      }
-
-      return await response.json();
-    } catch (error) {
-      console.error('Request failed:', error);
-      throw error;
+  async get(path) {
+    const resp = await fetch(this.baseURL + path);
+    if (!resp.ok) {
+      throw new Error(`GET ${path} failed: ${resp.status} ${resp.statusText}`);
     }
+    return await resp.json();
   }
 
-  get(endpoint, options) {
-    return this.request('GET', endpoint, options);
-  }
-
-  post(endpoint, options) {
-    return this.request('POST', endpoint, options);
-  }
-
-  put(endpoint, options) {
-    return this.request('PUT', endpoint, options);
-  }
-
-  delete(endpoint, options) {
-    return this.request('DELETE', endpoint, options);
-  }
-
-  patch(endpoint, options) {
-    return this.request('PATCH', endpoint, options);
+  async post(path, payload) {
+    const resp = await fetch(this.baseURL + path, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(payload),
+    });
+    if (!resp.ok) {
+      throw new Error(`POST ${path} failed: ${resp.status} ${resp.statusText}`);
+    }
+    return await resp.json();
   }
 }
